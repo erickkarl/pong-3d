7436406ce930106450a5908eac22bd16b43433f4
decarc <anderarcodaci@gmail.com>
2025-11-03 22:09:59 -0300

    feat: vfxApply

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -11,17 +11,14 @@ var move_up_key = KEY_UP
 @export
 var speed = 1
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if(Input.is_key_pressed(move_up_key)):
 		translate(Vector3(0,1*speed,0))
 		
 	if(Input.is_key_pressed(move_down_key)):
 		translate(Vector3(0,-1*speed,0))
+	
 	
